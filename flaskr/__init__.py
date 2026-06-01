f2c610a035f8fd6a68427709ad8603e4f8d54656
Zherish Galvin Mayordo <98146850+zhrssh@users.noreply.github.com>
2026-06-01 18:02:54 +0800

    Fixed some bugs

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,43 +5,49 @@ from flask import Flask
 from flask_cors import CORS, cross_origin
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
-def create_app(test_config = None) -> Flask:
+
+def create_app(test_config=None) -> Flask:
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    cors = CORS(app, resources={r"*": {"origins": ["http://localhost:3001"]}})
-    app.config['CORS_HEADERS'] = 'Content-Type'
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    cors = CORS(
+        app,
+        supports_credentials=True,
+        resources={r"*": {"origins": ["http://localhost:3001"]}},
     )
-    
+    app.config["CORS_HEADERS"] = "Content-Type"
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
-        
+
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
-    
+
     # healthcheck
-    @app.route('/health')
+    @app.route("/health")
     def healthcheck():
         """Used for health checking the app. Returns 200 if the app is healthy."""
         return "OK", 200
-    
-    
+
     from . import db
+
     db.init_app(app)
-    
+
     from . import admin
+
     app.register_blueprint(admin.bp)
-    
+
     return app
-    
